ec3d94c3de71f7b3deb09a76b5c7a4b012525b2e
Joon Lee <seouri@gmail.com>
2009-10-02 22:07:06 -0400

    added validations for Term

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,8 @@
 class Term < ActiveRecord::Base
   has_many :lines
+
+  validates_presence_of :string
+  validates_length_of :string, :minimum => 1
+  validates_format_of :string, :with => /^\S.+\S$/
+  validates_uniqueness_of :string
 end
