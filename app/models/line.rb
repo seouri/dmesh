c8d1e445d7ceccd8d062120ba2a8309db4e59b9a
Joon Lee <seouri@gmail.com>
2009-10-05 08:26:00 -0400

    the same term could appear on the same page

--- app/models/line.rb
+++ app/models/line.rb
@@ -7,7 +7,6 @@ class Line < ActiveRecord::Base
   validates_presence_of :page_id, :term_id, :tree_number
   validates_format_of :tree_number, :with => /^[A-Z](\d+)?(\.\d{1,3})*$/
   validates_uniqueness_of :tree_number, :scope => :book_id
-  validates_uniqueness_of :term_id, :scope => :page_id
 
   def level
     tree_number.length == 1 ? 0 : tree_number.split(/\./).size
